11b86e1af64bb9bcc47a315a9eae75bafb87885c
Ateja <atejukas123@gmail.com>
2023-06-24 15:19:33 +0100

    feat: compiled

--- server/controllers/messages.controller.ts
+++ server/controllers/messages.controller.ts
@@ -1,8 +1,8 @@
 import { Express, Request, Response } from 'express';
 import {
-  createMessageModel,
-  getMessagesModel,
-} from '../models/methods/messages.ts';
+  sendMessageModel,
+  getMessagesModel
+} from '../models/methods/messages';
 
 async function sendMessage(req: Request, res: Response) {
   try {
@@ -22,7 +22,7 @@ async function sendMessage(req: Request, res: Response) {
       receiver_name,
       date,
     };
-    const createMessage = await createMessageModel(newMessage);
+    const createMessage = await sendMessageModel(newMessage);
     res.status(201).json({
       message: 'Message sent successfully',
       result: createMessage,
