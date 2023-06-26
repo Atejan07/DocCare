3fc0c16c76a831619466cd6351ee95394d6475f3
Ateja <atejukas123@gmail.com>
2023-06-26 09:48:54 +0100

    feat: doctor login tested works

--- server/routers/doctor.route.ts
+++ server/routers/doctor.route.ts
@@ -17,9 +17,9 @@ const doctorRouter = Router();
 doctorRouter.post('/doctor/register', createDoctor);
 doctorRouter.post('/doctor/login', loginDoctor )
 doctorRouter.get('/doctor/:id', getDoctor);
-doctorRouter.get('/doctors', doctorAuthMiddleware, getDoctors);
-doctorRouter.post('/doctor/:id/medical-info', createMedicalInfo)
-doctorRouter.put('/doctor/summary', createPatientSummary);
+doctorRouter.get('/doctors', getDoctors);
+doctorRouter.post('/doctor/:id/medical-info',  doctorAuthMiddleware, createMedicalInfo)
+doctorRouter.put('/doctor/summary', doctorAuthMiddleware, createPatientSummary);
 
 export {doctorRouter};
 
