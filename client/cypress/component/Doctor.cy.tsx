39bc06223b21e2bfbe93a4d59937f72988a399da
Ateja <atejukas123@gmail.com>
2023-07-04 17:37:39 +0100

    feat: fixed env

--- client/cypress/component/Doctor.cy.tsx
+++ client/cypress/component/Doctor.cy.tsx
@@ -1,5 +1,54 @@
-// describe('Doctor.cy.tsx', () => {
-//   it('playground', () => {
-//     // cy.mount()
-//   })
-// })
+import React from "react";
+import Doctor from "@/app/doctor/dashboard/page";
+import MockRouter from "../fixtures/MockRouter";
+import 'cypress-localstorage-commands';
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
+
+describe("<Doctor>", () => {
+  beforeEach(() => {
+    cy.mount(
+      <MockRouter asPath="/doctor/dashboard">
+        <Provider store={store}>
+          <Doctor />
+        </Provider>
+      </MockRouter>
+    );
+  });
+  it("Renders the homepage", () => {
+    cy.get('main').should('be.visible');
+  });
+});
+
+
+
+
+// describe('<Home>', () => {
+//   it('Renders the homepage', () => {
+//     cy.get('h1').contains('Connect');
+//   });
+//   it('should scroll to the ref when the button is clicked', () => {
+//     cy.contains('See how it works').click();
+//     cy.get('main').should('be.visible');
+//   });
+// });
+// describe('<Users>', () => {
+//   it('should display three user options', () => {
+//     cy.get('.grid-users').within(() => {
+//       cy.get('.users-left').should('be.visible');
+//       cy.get('.users-center').should('be.visible');
+//       cy.get('.users-right').should('be.visible');
+//     });
+//   });
+//   it('should have links with the correct path', () => {
+//     cy.get('.grid-users').within(() => {
+//       cy.get('.users-left a').should('have.attr', 'href', '/patient/login');
+//       cy.get('.users-center a').should(
+//         'have.attr',
+//         'href',
+//         '/junior-doctor/login'
+//       );
+//       cy.get('.users-right a').should('have.attr', 'href', '/doctor/login');
+//     });
+//   });
+// });
--- client/cypress/component/Login.cy.tsx
+++ client/cypress/component/Login.cy.tsx
@@ -21,27 +21,6 @@ describe('<Login>', () => {
     cy.get('form input[name="password"]').should('exist');
     cy.get('form button[type="submit"]').should('exist');
   });
-  // it('should display an error message for invalid login', () => {
-  //   const invalidEmail = 'invalid-email@example.com';
-  //   const invalidPassword = 'wrong-password';
-  //   cy.get('form input[name="email"]').type(invalidEmail);
-  //   cy.get('form input[name="email"]').should('have.value', invalidEmail);
-  //   cy.get('form input[name="password"]').type(invalidPassword);
-  //   cy.get('form input[name="password"]').should('have.value', invalidPassword);
-  //   cy.get('form')
-  //     .submit()
-  //     .then(() => {
-  //       cy.request('POST', 'localhost:3001/patient/login', {
-  //         email: invalidEmail,
-  //         password: invalidPassword,
-  //         failOnStatusCode: false,
-  //       }).then((res) => {
-  //         console.log(res);
-  //         expect(res.status).to.equal(403);
-  //         expect(res.body.error).to.equal('Username or password not found');
-  //       });
-  //     });
-  // });
   it('should redirect to the dashboard after successful login', () => {
     const validEmail = 'carol123@example.com';
     const validPassword = '123';
